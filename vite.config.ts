1aba62b8bfbe2e8dbed68ba3b793ba761f265261
moonybuilder <130939050+moonybuilder@users.noreply.github.com>
2025-06-23 14:21:58 +0000

    vite.config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-pearlVentures/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  base: '/project-pearlVentures/',
+  }
 });
